5f69f8455d7e269c7553d5de7c75bbab6674562e
vibhu345 <vibhanshugupta875@gmail.com>
2026-06-22 19:15:56 +0530

    completed model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import Model_training_Config
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -41,6 +43,9 @@ if __name__== "__main__":
     train_data,test_data=obj.initiate_dat_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_tansformation(train_data,test_data)
+    train_array,test_array,_=data_transformation.initiate_data_tansformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array,test_array))
+
 
 
---
+++ src/components/model_trainer.py
@@ -0,0 +1,104 @@
+import os
+import sys
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from src.logger import logging
+from src.exception import CustomException
+from src.utills import save_object,evaluate_models
+
+@dataclass
+class Model_training_Config:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=Model_training_Config()
+    def initiate_model_trainer(self,final_train_data,final_test_data):
+        try:
+            logging.info("splitting the data into train and test")
+            X_train,Y_train,X_test,Y_test=(
+                final_train_data[:,:-1],
+                final_train_data[:,-1],
+                final_test_data[:,:-1],
+                final_test_data[:,-1]
+            )
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict= evaluate_models(X_train,Y_train,X_test,Y_test,models=models,params=params)
+            # to get best model score from dict
+            best_model_score= max(sorted(model_report.values()))
+            # to get best model name from dict
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model= models[best_model_name]
+            if best_model_score<0.6:
+                raise CustomException("No best found ")
+            logging.info("got the best model")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            r2score=r2_score(Y_test,predicted)
+            return r2score
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+
